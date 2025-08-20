4c320656711c90bcddfe9c5f7993c359069232ca
Isaac-Arinze <sunnyanas6@gmail.com>
2025-08-20 09:57:36 +0100

    Fixed webConfig

--- src/main/java/com/backend/property_plug/config/WebConfig.java
+++ src/main/java/com/backend/property_plug/config/WebConfig.java
@@ -10,26 +10,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public MultipartResolver multipartResolver() {
-        return new StandardServletMultipartResolver();
-    }
+	@Bean
+	public MultipartResolver multipartResolver() {
+		return new StandardServletMultipartResolver();
+	}
 
 
-     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                    "https://property-plug.vercel.app",
-                    "http://localhost:3000", // For development
-                    "http://127.0.0.1:5500"
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins(
+					"https://property-plug.vercel.app",
+					"http://localhost:3000", // For development
+					"http://127.0.0.1:5500"
 
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+				)
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+				.allowedHeaders("*")
+				.allowCredentials(true)
+				.maxAge(3600);
+	}
+
+}
 
 
 
